8c98f3e32639cd3e4d316dcb200d4b95cf17b120
wisnu518 <wisnuwisnu466@gmail.com>
2020-05-01 08:58:55 +0700

    P07

---
+++ src/mapreduce/koneksi.java
@@ -0,0 +1,25 @@
+package mapreduce;
+
+
+
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+/**
+ *
+ * @author Asus
+ */
+public class koneksi {
+
+    public static MongoDatabase sambungDB() {
+        try {
+            MongoClient client = MongoClients.create();
+            MongoDatabase database = client.getDatabase("bigdata");
+            return database;
+        } catch (Exception e) {
+            System.out.println("Koneksi Gagal: " + e.getMessage());
+        }
+        return null;
+    }
+}
